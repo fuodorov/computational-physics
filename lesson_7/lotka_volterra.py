f1c9f9930e89d78f3f8a12dfca3ce634a93ad1b3
Vyacheslav Fedorov <slava@fuodorov.ru>
2023-09-06 01:32:13 +0000

    Small fix lesson 7

--- lesson_7/lotka_volterra.py
+++ lesson_7/lotka_volterra.py
@@ -1,9 +1,9 @@
-
-from lesson_6.cauchy import runge_kutta
-import numpy as np
 import matplotlib.pyplot as plt
+import numpy as np
 
-if __name__ == '__main__':
+from lesson_6.cauchy import runge_kutta  # pylint: disable=import-error
+
+if __name__ == "__main__":
     print("Solve the Lotka-Volterra equations:")
     print("    dx/dt = a * x - b * x * y")
     print("    dy/dt = c * x * y - d * y")
@@ -14,24 +14,24 @@ if __name__ == '__main__':
 
     plt.figure(figsize=(10, 10), dpi=100)
     plt.grid()
-    
-    for x0, y0, color in [(1, 1, 'red'), (2, 2, 'blue'), (3, 3, 'green'), (4, 4, 'orange')]:
+
+    for x0, y0, color in [(1, 1, "red"), (2, 2, "blue"), (3, 3, "green"), (4, 4, "orange")]:
         for t in np.linspace(0, 50, 500):
             x = runge_kutta(
-                f=lambda t, x: np.array([a * x[0] - b * x[0] * x[1], c * x[0] * x[1] - d * x[1]]), 
-                x0=0, 
+                f=lambda t, x: np.array([a * x[0] - b * x[0] * x[1], c * x[0] * x[1] - d * x[1]]),
+                x0=0,
                 y0=np.array([x0, y0]),
-                x=t, 
-                h=1e-2, 
-                order=4
+                x=t,
+                h=1e-2,
+                order=4,
             )
             plt.scatter(x[0], x[1], color=color, s=1)
-            print(f"t = {t}, x = {x[0]}, y = {x[1]}", end='\r')
+            print(f"t = {t}, x = {x[0]}, y = {x[1]}", end="\r")
 
-        plt.scatter(x0, y0, color=color, s=10, marker='*', label=f'x0 = {x0}, y0 = {y0}')
-    
+        plt.scatter(x0, y0, color=color, s=10, marker="*", label=f"x0 = {x0}, y0 = {y0}")
+
     plt.legend()
-    plt.xlabel('Preys')
-    plt.ylabel('Predators')
-    plt.title('Lotka-Volterra equations')
-    plt.savefig('lesson_7/lotka_volterra.png')
+    plt.xlabel("Preys")
+    plt.ylabel("Predators")
+    plt.title("Lotka-Volterra equations")
+    plt.savefig("lesson_7/lotka_volterra.png")
