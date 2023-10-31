549769d156c0a639c99dee3d3f8841a53444ff8e
Vyacheslav Fedorov <slava@fuodorov.ru>
2023-10-31 14:11:39 +0000

    Update lesson 11

--- lesson_11/schrodinger.py
+++ lesson_11/schrodinger.py
@@ -1,69 +1,57 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+L, n = 15, 1000
+h = L / n
+k = 10
 
-def U(x):
-    return x**2 / 2
+A = np.zeros((n + 1, n + 1))
+psi = np.zeros((k + 1, n + 1))
+energy = np.zeros(k + 1)
+x = np.linspace(-L / 2, L / 2, n + 1)
+exact_solution = np.exp(-0.5 * x**2)
 
-
-n = 4000
-N = 60
-x1 = -10
-x2 = 10
-m = 4
-y0 = np.ones(n) + np.linspace(0, 1, n)
-x = np.linspace(x1, x2, n, True)
-h = x[1] - x[0]
-a = [-1.0 / (2 * h**2) for i in range(0, n)]
-b = [1.0 / h**2 + U(x[i]) for i in range(0, n)]
-c = [-1.0 / (2 * h**2) for i in range(0, n)]
+for i in range(n + 1):
+    psi[0][i] = i**2
 
 
-def tridiag_matrix_alg(a, b, c, d, n):
-    y = []
-    for i in range(0, n):
-        y.append(0)
-
-    for i in range(1, n):
-        xi = a[i] / b[i - 1]
-        a[i] = 0
-        b[i] -= xi * c[i - 1]
-        d[i] -= xi * d[i - 1]
-
-    y[n - 1] = d[n - 1] / b[n - 1]
-
-    for i in range(n - 2, -1, -1):
-        y[i] = 1 / b[i] * (d[i] - c[i] * y[i + 1])
-
-    return y
+def potential(x):
+    return 0.5 * x**2
 
 
-def inv_iter(y0, a, b, c, n, N, m):
-    psi, E = [], []
+for i, j in np.ndindex(A.shape):
+    if i == j:
+        A[i][j] = 1 / h**2 + potential(x[i])
+    elif i == j - 1:
+        A[i][j] = -1 / (2 * h**2)
+    elif i == j + 1:
+        A[i][j] = -1 / (2 * h**2)
 
-    for j in range(0, m):
-        d2 = y0.copy()
-        for k in range(0, j):
-            d2 = d2 - psi[k] * (np.inner(y0, psi[k])) / np.linalg.norm(psi[k])
-        for i in range(0, N):
-            d1 = d2
-            d = d1.copy()
-            a1 = a.copy()
-            b1 = b.copy()
-            c1 = c.copy()
-            d2 = tridiag_matrix_alg(a1, b1, c1, d, n)
-            for k in range(0, j):
-                d2 = d2 - psi[k] * (np.inner(d2, psi[k])) / np.linalg.norm(psi[k])
+A[0][0] = 1 / h**2 + potential(x[0])
+A[0][1] = -1 / (2 * h**2)
+A[n][n - 1] = -1 / (2 * h**2)
+A[n][n] = 1 / h**2 + potential(x[n])
 
-        E.append(np.linalg.norm(d1) / np.linalg.norm(d2))
-        psi.append(d2 / np.linalg.norm(d2))
-    return [E, psi]
+A_inv = np.linalg.inv(A)
 
 
-[E, psi] = inv_iter(y0, a, b, c, n, N, m)
+for i in range(1, k + 1):
+    psi[i] = A_inv.dot(psi[i - 1])
+    energy[i] = np.linalg.norm(psi[i - 1]) / np.linalg.norm(psi[i])
 
-for i in range(0, m):
-    plt.plot(x, psi[i], label=f"$\psi_{i + 1}$, E = {E[i].round(3)}")
 
+plt.plot(
+    x,
+    psi[-1] / np.linalg.norm(psi[-1]),
+    linestyle="--",
+    color="r",
+    alpha=0.5,
+    label=f"$\psi_{0}$, $E_{0}$ = {energy[-1]:.1f}",
+)
+plt.plot(
+    x, exact_solution / np.linalg.norm(exact_solution), linestyle="-", color="b", alpha=0.5, label="Exact solution"
+)
 plt.legend()
+plt.grid()
+
 plt.savefig("lesson_11/schrodinger.png")
